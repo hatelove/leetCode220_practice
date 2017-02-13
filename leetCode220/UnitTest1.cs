fb0862a648472a731ce80b921e73c8e1b9f84901
hatelove <jay.hatelove@gmail.com>
2017-02-13 15:24:23 +0800

    initialized and add a failed test: k=0, should return false;

---
+++ leetCode220/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetCode220
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void k_is_0_should_return_false()
+        {
+            var nums = new int[] { 9 };
+            var k = 0;
+            var t = 0;
+            Assert.IsFalse(new Solution().ContainsNearbyAlmostDuplicate(nums, k, t));
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// 給定整數數組，找出在陣列中是否存在兩個不同的索引i和j，
+        /// 使得nums [i]和nums [j]之間的絕對差最大為t，
+        /// 並且i和j之間的絕對差最大k。
+        /// </summary>
+        /// <param name="nums">The nums.</param>
+        /// <param name="k">The k.</param>
+        /// <param name="t">The t.</param>
+        /// <returns></returns>
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
